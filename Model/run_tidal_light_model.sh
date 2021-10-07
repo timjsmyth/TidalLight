4dc376e0329d862cb695d72f6f4cad9d2c37480d
Tim Smyth <tjsm@pml.ac.uk>
2021-10-07 11:15:38 +0100

    Run for global cities for 2020

--- Model/run_tidal_light_model.sh
+++ Model/run_tidal_light_model.sh
@@ -2,7 +2,7 @@
 ## declare an array variable holding the locations
 
 declare -a locations=("Tokyo" "Eilat" "Plymouth_Dockyard" "Lagos" "NewYork" "LosAngeles" "BuenosAires" "Shanghai" "Mumbai")
-declare -a locations=("Plymouth_Dockyard")
+#declare -a locations=("Plymouth_Dockyard")
 
 # Datum fraction (e.g. 0.5 = 50%)
 DP=0.25
@@ -11,16 +11,20 @@ DP=0.25
 TI=0.25
 
 # Start date
-START="2021-09-28"
+START="2020-01-01"
 # End date
-END="2021-10-10"
+END="2020-12-31"
 
 ## loop through the locations array
 for location in "${locations[@]}"
 do
    echo "$location"
    # Run the TidalLight_Model.py
-   /bin/python3 TidalLight_Model.py -s -A 2 -l -t -T $TI -dp $DP -o -loc $location -p -start $START -end $END #-TC
+   ## This version if you want to plot (i.e. short duration runs)
+   #/bin/python3 TidalLight_Model.py -s -A 2 -l -t -T $TI -dp $DP -o -loc $location -p -start $START -end $END #-TC
+   ## This version without plots (long runs)
+   /bin/python3 TidalLight_Model.py -s -A 2 -l -t -T $TI -dp $DP -o -loc $location -start $START -end $END #-TC
+
 done
 
 exit 0
